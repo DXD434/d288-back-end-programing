1fb2ea6659c0eb204e569615d6c220505cca9f77
Admin <ddarkoh@wgu.edu>
2025-06-07 21:54:02 -0700

    Completed G

--- src/main/java/com/example/demo/entities/Customer.java
+++ src/main/java/com/example/demo/entities/Customer.java
@@ -1,123 +1,25 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import java.util.Date;
+
+import java.util.HashSet;
+
 import java.util.Set;
 
 
 @Entity
 @Table(name = "customers")
 public class Customer {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
+    @Column(name = "customer_id", nullable = false)
     private Long id;
 
-    @Column(name = "address")
-    private String address;
-
-    @Column(name = "create_date")
-    @CreationTimestamp
-    private Date create_date;
-
-    @Column(name = "customer_first_name")
-    private String firstName;
-
-    @Column(name = "customer_last_name")
-    private String lastName;
-
-    @Column(name = "last_update")
-    @UpdateTimestamp
-    private Date last_update;
-
-    @Column(name = "phone", nullable = false)
-    private String phone;
-
-    @Column(name = "postal_code", nullable = false)
-    private String postal_code;
-
-    @ManyToOne
-    @JoinColumn(name = "division_id", nullable = false)
-    private Division division;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer", fetch = FetchType.LAZY)
-    private Set<Cart> carts;
+    private Set<Cart> carts = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Date getCreate_date() {
-        return create_date;
-    }
-
-    public void setCreate_date(Date create_date) {
-        this.create_date = create_date;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getLast_update() {
-        return last_update;
-    }
-
-    public void setLast_update(Date last_update) {
-        this.last_update = last_update;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPostal_code() {
-        return postal_code;
-    }
-
-    public void setPostal_code(String postal_code) {
-        this.postal_code = postal_code;
-    }
-
-    public Division getDivision() {
-        return division;
-    }
-
-    public void setDivision(Division division) {
-        this.division = division;
-    }
+    // ... other getters and setters ...
 
     public Set<Cart> getCarts() {
         return carts;
@@ -127,10 +29,11 @@ public class Customer {
         this.carts = carts;
     }
 
-    public void add(Cart cart) {
-        this.carts.add(cart);
-    }
-
+    // Convenience method to add a Cart and keep bidirectional consistency
     public void addCart(Cart cart) {
+        if (cart != null) {
+            carts.add(cart);
+            cart.setCustomer(this);
+        }
     }
 }
